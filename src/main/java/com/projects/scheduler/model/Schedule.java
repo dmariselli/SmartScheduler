22da3b275cdaa63d4148eaded1e6404364314dd1
dmariselli <dmariselli@gmail.com>
2015-10-30 19:32:55 -0400

    Added applicationContext for Spring and added necessary Hibernate properties. Added another model class.

--- src/main/java/com/projects/scheduler/model/Schedule.java
+++ src/main/java/com/projects/scheduler/model/Schedule.java
@@ -17,7 +17,9 @@ public class Schedule {
 
     private byte daysOfWeek;  // 00000000 == 0SMTWThFS
 
-    private byte[] startEnd = new byte[7]; // 00000000 == 00:00-00:00 where index 0 corresponds to Sunday, 1 to Monday, etc.
+    // 00000000 == 00:00-00:00 where index 0 corresponds to Sunday, 1 to Monday, etc.
+    // Get top 4 by integer / by 10000. Get bottom 4 by % 10000
+    private byte[] startEnd = new byte[7];
 
     public int getDuration() {
         // TODO If the duration of the class varies on the day, perhaps return 0? Change to Integer and return null?
